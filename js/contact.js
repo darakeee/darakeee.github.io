8adb3b0b17ef4e32fa772a109ad51e93a145ab2f
darakeee <Prism.168.UandME@gmail.com>
2021-09-15 22:02:24 +0900

    Update 0915 2202

---
+++ js/contact.js
@@ -0,0 +1,21 @@
+$(function(){
+	var click=true;
+    $(".btn-navi").on("click", function(){
+		var rightVal = 0;
+		if(click){
+			//menuボタンを押したときの動作
+			if($(this).hasClass("open")){
+				$(this).removeClass("open");
+				rightVal = -300;
+			}
+			else{
+				$(this).addClass("open");
+			}
+
+			click=false;
+			$("#global-navi").animate({'right':rightVal}, 500, function(){
+				click=true;
+			});
+		}
+	});
+});
